62970bfb760d237810b0ef5312cfe3e47b1818cc
sshahbscs <sshah.bscs22seecs@seecs.edu.pk>
2024-02-11 22:28:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function search() {
+    var input = document.getElementById('searchInput').value;
+    // Perform search operation, you can define your own logic here
+    alert('You searched for: ' + input);
+}
